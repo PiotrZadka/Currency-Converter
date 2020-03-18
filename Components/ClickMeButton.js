4d917aee004122c244b2322956a5798184eff877
PiotrZadka <piotr.zadka@gmail.com>
2020-03-18 13:14:02 +0000

    added button

---
+++ Components/ClickMeButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from 'react-native';
+
+const ClickMeButton = () =>{
+    return(
+            <Button title={"Click me!"}></Button>
+    )
+}
+
+export default ClickMeButton
